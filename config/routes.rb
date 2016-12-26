4e9f32248fa047ac44fd4b23c24908e3b3100cbb
Alex Lopatin <lopatin.rnd@gmail.com>
2016-12-26 16:24:07 +0300

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+
+  root 'demo#index' #root route, demo - controller's name; index - action
+  get 'demo/index' #this is simple route - should match exactly
+
+  #default ruote - could be obsolete with new versions of rails
+  #get ':controller(/:action(/:id))'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
