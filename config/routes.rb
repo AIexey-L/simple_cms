7330b1f7295a7030803aee39b3bea05c442f9425
Alex Lopatin <lopatin.rnd@gmail.com>
2017-01-06 11:51:19 +0300

    added links examples

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   root 'demo#index' #root route, demo - controller's name; index - action
   get 'demo/index' #this is simple route - should match exactly
+  get 'demo/hello'
 
   #default ruote - could be obsolete with new versions of rails
   #get ':controller(/:action(/:id))'
